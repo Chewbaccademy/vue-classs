f1660ae6b43b24a4e57cef1137fc7cd5e51509ff
chewbaccademy <kilian.debraux@lacatholille.fr>
2021-02-02 14:26:03 +0100

    exo fini

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,46 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <div id="listDiv">
+    <h1 v-if="showText"> {{text}} </h1>
+    <ul v-else id="list">
+      <li v-for="item in items" v-bind:key="item"> {{item.id}} : {{item.nom}}</li>
+    </ul>
+    <button @click="toogleTextList">changer l'affichage</button>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      text : "bienvenu sur l'appli",
+      showText: true,
+      items: [{id: 13, nom: "hervé"}, {id: 26, nom: "Pascal"}, {id: 32, nom: "Eglantine"}]
+    }
+  },
+  methods: {
+    toogleTextList() {
+      this.showText = !this.showText;
+    }
   }
 })
+
 </script>
 
 <style>
+#listDiv {
+  position: absolute;
+  top: 50vh;
+  left: 50vw;
+  transform: translate(-50%,-50%);
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  width: 20vw;
+  justify-content: space-around;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
